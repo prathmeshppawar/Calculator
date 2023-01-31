c7c9e1c1a00bb7622db04ba827730776c55d43ef
Prathmesh Pawar <prathmeshpawar@Prathmeshs-MacBook-Air.local>
2023-01-31 14:06:25 +0530

    Calculator First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [currOperand, setCurrOperand] = useState("");
+  const [prevOperand, setPrevOperand] = useState("");
+  const handleDigit = (num) =>{
+    setCurrOperand(currOperand + "" + num);
+  }
+  const handleOperation = (action) =>{
+    if(currOperand[currOperand.length-1]==="+"||currOperand[currOperand.length-1]==="-"||currOperand[currOperand.length-1]==="*"||currOperand[currOperand.length-1]==="/"){
+      var str = currOperand;
+      var result = str.toString().substring(0, str.toString().length - 1);
+      setCurrOperand(result+action);
+    }else{
+      setCurrOperand(currOperand+action);
+    }
+  }
+  const handleSolution = () =>{
+    if(currOperand[currOperand.length-1]==="+"||currOperand[currOperand.length-1]==="-"||currOperand[currOperand.length-1]==="*"||currOperand[currOperand.length-1]==="/"){
+      var str = currOperand;
+      var result = str.toString().substring(0, str.toString().length - 1);
+      setPrevOperand(result);
+      const sol = eval(result);
+      setCurrOperand(sol);
+    }else{
+      setPrevOperand(currOperand);
+      const sol = eval(currOperand);
+      setCurrOperand(sol);
+    }
+  }
+  const clearFunc = () =>{
+    setPrevOperand("");
+    setCurrOperand("");
+  }
+  const delFunc = () =>{
+    var str = currOperand;
+    var result = str.toString().substring(0, str.toString().length - 1);
+    setCurrOperand(result);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator-grid">
+        <div className="output">
+        <div className="previous-operand">{prevOperand}</div>
+          <div className="current-operand">{currOperand}</div>
+        </div>
+        <button className="span-two" onClick={clearFunc}>AC</button>
+        <button className="digit" onClick={delFunc}>DEL</button>
+        <button className="operation" onClick={()=>{handleOperation("/")}}>÷</button>
+        <button className="digit" onClick={()=>{handleDigit(1)}}>1</button>
+        <button className="digit" onClick={()=>{handleDigit(2)}}>2</button>
+        <button className="digit" onClick={()=>{handleDigit(3)}}>3</button>
+        <button className="operation" onClick={()=>{handleOperation("*")}}>*</button>
+        <button className="digit" onClick={()=>{handleDigit(4)}}>4</button>
+        <button className="digit" onClick={()=>{handleDigit(5)}}>5</button>
+        <button className="digit" onClick={()=>{handleDigit(6)}}>6</button>
+        <button className="operation" onClick={()=>{handleOperation("+")}}>+</button>
+        <button className="digit" onClick={()=>{handleDigit(7)}}>7</button>
+        <button className="digit" onClick={()=>{handleDigit(8)}}>8</button>
+        <button className="digit" onClick={()=>{handleDigit(9)}}>9</button>
+        <button className="operation" onClick={()=>{handleOperation("-")}}>-</button>
+        <button className="digit" onClick={()=>{handleDigit(".")}}>.</button>
+        <button className="digit" onClick={()=>{handleDigit(0)}}>0</button>
+        <button className="span-two" onClick={()=>{handleSolution()}}>=</button>
+      </div>
     </div>
   );
 }
